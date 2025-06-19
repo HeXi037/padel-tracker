4f798c844c6934b2f12d781ffff48d2dccad1359
HeXi037 <emilglad037@gmail.com>
2025-06-20 01:06:17 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth";
-import matchRoutes from "./routes/match";
+import authRoutes from "./routes/auth.js";
+import matchRoutes from "./routes/match.js";
 
 dotenv.config();
 const app = express();
